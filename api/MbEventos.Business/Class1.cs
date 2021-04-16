d8b22522d8b503477eb4bcaa1009ed2b889da5d1
Rafael Oliveira <rafaelntn766@gmail.com>
2021-04-15 21:14:18 -0300

    removendo arquivo

--- api/MbEventos.Business/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MbEventos.Business
-{
-    public class Class1
-    {
-    }
-}
--- api/MbEventos.Repositories/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MbEventos.Repositories
-{
-    public class Class1
-    {
-    }
-}
